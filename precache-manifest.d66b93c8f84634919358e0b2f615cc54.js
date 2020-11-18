8b9d3681d266b90446ba4d7ac2c3641cd0629de9
hasifazhaan <hasif45azhaan@gmail.com>
2020-11-18 20:30:51 +0530

    Updates

---
+++ precache-manifest.d66b93c8f84634919358e0b2f615cc54.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c551213eab047f38665f171522aa73cc",
+    "url": "/MusicApp/index.html"
+  },
+  {
+    "revision": "aa1c3d24b5e5c51f8f8f",
+    "url": "/MusicApp/static/css/main.4b9bf767.chunk.css"
+  },
+  {
+    "revision": "dc09b4678078e58fcfb2",
+    "url": "/MusicApp/static/js/2.cfcd1ca6.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/MusicApp/static/js/2.cfcd1ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1c3d24b5e5c51f8f8f",
+    "url": "/MusicApp/static/js/main.9cb1bd21.chunk.js"
+  },
+  {
+    "revision": "c89677f2e2170749934e",
+    "url": "/MusicApp/static/js/runtime-main.a8042234.js"
+  },
+  {
+    "revision": "4abaf1a16bc0d7198eebbd0fb36a6d5a",
+    "url": "/MusicApp/static/media/Bhula.4abaf1a1.mp3"
+  },
+  {
+    "revision": "25926667a7fe2d50a49b54b2eebce2a3",
+    "url": "/MusicApp/static/media/Kaash_aisa_hota.25926667.mp3"
+  },
+  {
+    "revision": "d93c45e564109a6bcea5cfc1b1b2c9d4",
+    "url": "/MusicApp/static/media/Saari_ki_saari.d93c45e5.mp3"
+  },
+  {
+    "revision": "0a76f2ab3493b64e6115b18b8b6726b8",
+    "url": "/MusicApp/static/media/aaya_na_tu.0a76f2ab.mp3"
+  },
+  {
+    "revision": "cc1016be47ead72a4f1eab470d106467",
+    "url": "/MusicApp/static/media/dil_chahte_ho.cc1016be.mp3"
+  },
+  {
+    "revision": "ff4fff19f3f91784499ed0248a99bc8b",
+    "url": "/MusicApp/static/media/dil_chahte_ho.ff4fff19.jpg"
+  },
+  {
+    "revision": "16349c29ac2bf7e57446c3a4b3696997",
+    "url": "/MusicApp/static/media/hawa_banke.16349c29.jpg"
+  },
+  {
+    "revision": "bcc61f311db1d7c122507d85cc81b546",
+    "url": "/MusicApp/static/media/hawa_banke.bcc61f31.mp3"
+  },
+  {
+    "revision": "21351f49c5b1ce3c89570dc4e9dc33c2",
+    "url": "/MusicApp/static/media/kuch_to_bata.21351f49.jpg"
+  },
+  {
+    "revision": "8c9e456bca87f43d8892ec176946a471",
+    "url": "/MusicApp/static/media/kuch_to_bata.8c9e456b.mp3"
+  },
+  {
+    "revision": "61dd07cab86c4c4e7020b1426ffe9ba9",
+    "url": "/MusicApp/static/media/logo.61dd07ca.jpg"
+  },
+  {
+    "revision": "7c7f68988f9e78a453b54da2b728acd8",
+    "url": "/MusicApp/static/media/tujhe_kitna_chahe_aur_hum.7c7f6898.mp3"
+  },
+  {
+    "revision": "a760e2b47593933cec593fbc227a8eb6",
+    "url": "/MusicApp/static/media/tujhe_kitna_chahe_aur_hum.a760e2b4.jpg"
+  }
+]);
